bf10c3a9a50cedb001b78f8ff267b8e38cb72f77
Noobizard <jjyoon130@naver.com>
2016-10-20 09:51:52 +0900

    i'm change this

--- hello.c
+++ hello.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
-#include <stblib.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
 
 int main(void) {
 
 	printf("hello\n");
 	printf("hello2\n");
+	int i;
+
+	for(i = 0 ; i < 5 ; i++)
+	{
+		printf("time is running out --> %d \n",i);
+		sleep(1);
+	}
 }
